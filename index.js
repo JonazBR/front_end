093f203d2c01e02626a570d40efff1b32e771646
EmanoelV <nanerarieiv4@gmail.com>
2019-10-14 19:28:23 -0300

    add menu dinamico

---
+++ index.js
@@ -0,0 +1,12 @@
+var menu = document.getElementById("menu")
+var buttom = document.getElementById("menu-dinamico")
+
+buttom.addEventListener("click", () => {
+    if(menu.classList.value == "menu_dinamico_nav") {
+        menu.classList.value = menu.classList.value + " deitado"
+    } else {
+        console.log("else")
+        menu.classList.value = "menu_dinamico_nav"
+    }
+    
+})
