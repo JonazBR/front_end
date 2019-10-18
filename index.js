c9fa1ba66fe4d9f00a55561f989dedbf69793a7d
honny batista de oliveira <honnyevol@gmail.com>
2019-10-18 20:43:24 -0300

    paleativo para o bug do footer colando no hearder

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var buttom = document.getElementById("menu-dinamico")
 buttom.addEventListener("click", () => {
     if(menu.classList.value == "menu_dinamico_nav") {
         menu.classList.value = menu.classList.value + " deitado"
+        
     } else {
         console.log("else")
         menu.classList.value = "menu_dinamico_nav"
